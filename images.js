9be5bc4795207552136e1e519584e4dfbc0b3d1d
Jonas O <jonaso@web.de>
2015-06-05 15:07:13 +0100

    fix duplicate image download

--- images.js
+++ images.js
@@ -16,18 +16,36 @@ function dl_img (e) {
 
 	var w = window.localStorage.getItem('img_width');
 	var h = window.localStorage.getItem('img_height');
-
 	if(!w) { w = 160; }
 	if(!h) { h = 120; }
 
-	if($(this).width() > w && $(this).height() > h)
-	{
-	    dnl_downloadImg($(this).attr("src"));
+	if($(this).is('img')) {
+		if($(this).width() > w && $(this).height() > h)
+		{
+			dnl_downloadImg($(this).attr("src"));
+		}
+	} else {
+
+		bg_img_src = false;
+		var match = (window.getComputedStyle(this).getPropertyValue("background-image")).match(/url\(([^)]+)\)/i);
+		if (match) {
+			bg_img_src = match[1];
+		}
+		if (!bg_img_src) {
+			return;
+		}
+		if($(this).width() > w && $(this).height() > h)
+		{
+		    dnl_downloadImg(bg_img_src);
+		}
 	}
 
 }//dl_img
 
+/*
 function dl_div (e) {
+
+	console.log('dragged div');
 
 	//check if there is a background image on this div container
 	bg_img_src = false;
@@ -51,6 +69,6 @@ function dl_div (e) {
 	}
 
 }//dl_div
+*/
 
-$(document).on('dragend', 'img', dl_img);
-$(document).on('dragend', 'div', dl_div);
+$(document).on('dragend', '*', dl_img);
